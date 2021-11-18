cdb381310cf7464185e56126fce77d34b7f5a90b
tobiasuruali <tobias.hoesli@hotmail.com>
2021-11-18 13:06:01 +0100

    functioning save_array_to_db functionality impl

--- db/save_array_to_db.py
+++ db/save_array_to_db.py
@@ -16,7 +16,7 @@ cursor.execute("DROP TABLE IF EXISTS input_data")
 
 cursor.execute(
     """
-    CREATE TABLE input_data (
+    CREATE TABLE IF NOT EXISTS input_data (
         id INT NOT NULL,
         name VARCHAR,
         x_test_set BYTEA,
@@ -31,6 +31,13 @@ x_train = np.random.rand(1500,550)
 id = '1'
 name = 'Training Set X'
 
+# sql_insert_array = """
+#     INSERT INTO input_data (id, name, x_test_set)
+#     VALUES (%s,%s,%s)
+#     """
+# sql_array = (i[0], 'X Training Set', x_train)
+
+#Persist training set into the db
 cursor.execute(
     """
     INSERT INTO input_data (id, name, x_test_set)
@@ -39,3 +46,19 @@ cursor.execute(
     (id, name, pickle.dumps(x_train))
 )
 
+#get array back from db
+id = '1'
+cursor.execute(
+    """
+    SELECT x_test_set
+    FROM input_data
+    WHERE id=%s
+    """,
+    (id,)
+)
+x_train_from_db = pickle.loads(cursor.fetchone()[0])
+
+print(x_train_from_db.shape)
+print(x_train.shape)
+conn.commit()
+conn.close()
