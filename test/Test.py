0435c573292528d4ddd3de0b5a7557b5e599ad57
tobiasuruali <tobias.hoesli@hotmail.com>
2023-05-18 12:35:38 +0200

    added tag to our main dstoolkits app. deleted images no more necessary in this depository.

--- test/Test.py
+++
@@ -1,8 +0,0 @@
-print("Hello World")
-
-lines = ['Readme', 'How to write text files in Python']
-with open('readme.txt', 'w') as f:
-    for line in lines:
-        f.write(line)
-        f.write('\n')
-        print('Text succesfully added to file')
